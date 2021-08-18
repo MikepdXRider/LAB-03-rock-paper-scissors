33b3e71cb1ecc4d8ec202e251868b7c6b981484a
Michael Rider <mikepdxrider@gmail.com>
2021-08-18 14:28:42 -0700

    grabs DOM elements and initializes state.

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // import functions and grab DOM elements
+const gameCounterEl = document.querySelector('#win-loss-draw-counter');
+const throwButtonEl = document.querySelector('#throw-button');
+const resetButtonEl = document.querySelector('#reset-button');
+const textOutputEl = document.querySelector('#text-output');
+console.log(gameCounterEl, throwButtonEl, resetButtonEl, textOutputEl);
+
 
 // initialize global state
+numberOfWins = 0;
+numberOfLosses = 0;
+numberOfDraws = 0;
 
 // set event listeners 
   // get user input
