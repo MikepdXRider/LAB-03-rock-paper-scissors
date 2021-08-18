0488d2b2798d3465c77d5b2be47126b5adb5bc64
Michael Rider <mikepdxrider@gmail.com>
2021-08-18 15:34:41 -0700

    abstract displayText(s), disableThrowEnableReset, and updateGameCoutner functions.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { getRandomThrow } from './get-random-throw.js';
-import { didUserWin } from './utils.js';
+import { didUserWin, displayDrawText, displayLossText, displayWinText, updateGameCounter } from './utils.js';
 
 // import functions and grab DOM elements
 const gameCounterEl = document.querySelector('#win-loss-draw-counter');
@@ -12,11 +12,24 @@ const textOutputEl = document.querySelector('#text-output');
 // console.log(getRandomThrow());
 
 // initialize global state
-numberOfWins = 0;
-numberOfLosses = 0;
-numberOfDraws = 0;
+let numberOfWins = 0;
+let numberOfLosses = 0;
+let numberOfDraws = 0;
 
 // set event listeners 
+throwButtonEl.addEventListener('click', () => {
+    let correctAnswer = getRandomThrow();
+    console.log(correctAnswer);
+
+    const userSelectedInput = document.querySelector('input:checked').value;
+    console.log(userSelectedInput);
+
+  // if (didUserWin === 'win'){
+  //   // - Update text field.
+  //   // - Increment numberOfWins.
+  //   // - Update win/loss counter. 
+  // }
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- utils.js
+++ utils.js
@@ -10,3 +10,24 @@ export function didUserWin(userThrow, computerThrow) {
     if (userThrow === 'scissors' && computerThrow === 'paper') return 'win';
     if (userThrow === 'scissors' && computerThrow === 'rock') return 'lose';
 }
+
+export function disableThrowEnableReset(element1, element2){
+    element1.disable = true;
+    element2.style.display = 'block';
+}
+
+export function displayWinText(element1, computerThrow) {
+    element1.textContent = `The computer threw ${computerThrow}. You win!`;
+}
+
+export function displayDrawText(element1) {
+    element1.textContent = `The computer threw ${computerThrow}. It's a draw!`;
+}
+
+export function displayLossText(element1) {
+    element1.textContent = `The computer threw ${computerThrow}. You lose!`;
+}
+
+export function updateGameCounter(element1, wins, losses, draws) {
+    element1.textContent = `Win / Loss / Draw : ${wins} / ${losses} / ${draws}`;
+}
